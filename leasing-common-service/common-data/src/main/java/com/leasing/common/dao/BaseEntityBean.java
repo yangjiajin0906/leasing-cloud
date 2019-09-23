f12db49d4e6fed2a2ad56c633ff672f6ffa8ed2b
lvchennan <540819414@qq.com>
2019-09-23 14:03:37 +0800

    增加系统管理模块

--- leasing-common-service/common-data/src/main/java/com/leasing/common/dao/BaseEntityBean.java
+++ leasing-common-service/common-data/src/main/java/com/leasing/common/dao/BaseEntityBean.java
@@ -4,7 +4,10 @@ package com.leasing.common.dao;
  * @project:leasing-cloud
  * @date:2019-9-23
  * @author:lvcna@yonyou.com
- * @description:
+ * @description: 业务模型基类
  **/
 public class BaseEntityBean {
+
+
+
 }
